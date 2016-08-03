1e7b7c0c036a97b40f3a377edd98fbad9494ffa3
Grando F <aqdevmain@gmail.com>
2016-08-03 11:47:15 -0700

    jfltespr: Remove libstlport
    Move up to common
    
    Change-Id: Ief63b6905acf8bc62c8420073c509151f6043d4b

--- full_jfltespr.mk
+++ full_jfltespr.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
+
 #
 # This file is the build configuration for a full Android
 # build for maguro hardware. This cleanly combines a set of
@@ -36,7 +36,3 @@ PRODUCT_MODEL := SPH-L720
 # Qualcomm
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
-
-# Stlport
-PRODUCT_PACKAGES += \
-    libstlport
