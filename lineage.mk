4dd6b71aa4c1a2882790bc81df6a030dde1f749e
Nolen Johnson <johnsonnolen@gmail.com>
2018-07-07 21:58:24 -0400

    jfltespr: Migrate from vendor/cm to vendor/lineage
    * As of Lineage 15.0 (and the rebrand associated), this is the new vendor.
    
    Change-Id: Ie1f70d034f8beff144fec7ebb4e9aacd54dda479

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 4d9120f66ed1e0f6ddc308f514aa4b48a77aff30PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
