852d9dd2375962320b4dae6ca54db86e75a185db
Bruno Martins <bgcngm@gmail.com>
2018-12-18 11:05:57 +0000

    jfltespr: Consider lineage_jfltevzw product via the AOSP way
    Change-Id: If38445c9301997bd5247d44ffd0da937f1154ae1

--- lineage_jfltespr.mk
+++ lineage_jfltespr.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-4d9120f66ed1e0f6ddc308f514aa4b48a77aff30PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
     TARGET_DEVICE=jfltespr \
     PRIVATE_BUILD_DESC="jfltespr-user 5.0.1 LRX22C L720VPSGPL1 release-keys"
