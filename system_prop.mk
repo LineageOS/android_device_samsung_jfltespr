711f55bb751d7a3ca04e03db88055674bf45c180
Arne Coucheron <arco68@gmail.com>
2019-06-16 06:52:20 +0000

    jfltespr: Migrate system.prop to system_prop.mk
    * Build system inclusion of system.prop will be deprecated soon,
      so swap to setting them via makefile.
    
    Change-Id: Iaa67b9dccab1213cb6ddc58f864820853b037484

---
+++ system_prop.mk
@@ -0,0 +1,16 @@
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2016-09-01
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.snapshot_enabled=1 \
+    persist.radio.snapshot_timer=22 \
+    ril.subscription.types=NV,RUIM \
+    ro.cdma.home.operator.alpha=Sprint \
+    ro.cdma.home.operator.numeric=310120 \
+    ro.telephony.default_network=10 \
+    ro.telephony.default_cdma_sub=0 \
+    telephony.lteOnCdmaDevice=1 \
+    ro.config.vc_call_vol_steps=7 \
+    DEVICE_PROVISIONED=1
