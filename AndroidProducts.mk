15fb8ef6d961251c345b5796059910485645d032
Arne Coucheron <arco68@gmail.com>
2019-06-16 06:46:06 +0000

    jfltespr: Deprecate full_jfltespr in favor of the lineage target
    * Update Copyrights.
    
    Change-Id: I7d3c8dd77fcc05e25c628858fb1f67a4e450c438

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +13,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_jfltespr.mk \
     $(LOCAL_DIR)/lineage_jfltespr.mk
