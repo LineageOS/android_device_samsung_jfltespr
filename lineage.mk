b4e47b94718c99729b5b7b3013513b528e7d6851
Nolen Johnson <johnsonnolen@gmail.com>
2019-07-02 16:07:39 +0000

    jfltespr: Fix vendor image mismatch
    * Needed after https://review.lineageos.org/#/c/208868/
    
    Change-Id: Id2e06ba25f8ba45a8574fb91d78fb752db808942

--- lineage.mk
+++ lineage.mk
@@ -3,13 +3,14 @@ $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
+4d9120f66ed1e0f6ddc308f514aa4b48a77aff30PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
     TARGET_DEVICE=jfltespr \
-    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:5.0.1/LRX22C/L720VPSGPL1:user/release-keys" \
     PRIVATE_BUILD_DESC="jfltespr-user 5.0.1 LRX22C L720VPSGPL1 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+BUILD_FINGERPRINT := "samsung/jfltespr/jfltespr:5.0.1/LRX22C/L720VPSGPL1:user/release-keys"
 
 PRODUCT_NAME := lineage_jfltespr
 PRODUCT_DEVICE := jfltespr
